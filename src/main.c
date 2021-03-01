58ec68431f2e5cdefc846c27b08f56634e96ebc6
Raphael Dray <dray.raph@gmail.com>
2021-03-01 21:00:17 +0100

    Prepared main for coding the XOR function and fixed warnings issue

--- src/main.c
+++ src/main.c
@@ -9,9 +9,58 @@
 //-------------------------------------------------------------------------------------------------
 //  Chiffrement et déchiffrement
 
-int main()
+int main(int argc, char ** argv)
 {
-    CipherData cipherData = { 0 };
+    // panda cipher :D!
+    Block plaintext = 
+    { 
+        .bundles = 
+        {
+            0x70, 0x61, 0x6e, 0x64, 
+            0x61, 0x20, 0x63, 0x69, 
+            0x70, 0x68, 0x65, 0x72, 
+            0x20, 0x3a, 0x44, 0x21
+        }
+    };
+
+    printf("Plain  ");
+    printBlock(&plaintext);
+
+    // 0x0123456789ABCDEF
+    Block cipherKey = 
+    { 
+        .bundles = 
+        {
+            0x0, 0x1, 0x2, 0x3, 
+            0x4, 0x5, 0x6, 0x7, 
+            0x8, 0x9, 0xA, 0xB, 
+            0xC, 0xD, 0xE, 0xF
+        }
+    };
+
+    printf("Key    ");
+    printBlock(&cipherKey);
+
+    printf("\n[+] XORing with the cipherkey..\n");
+    Block_xor(&plaintext, &cipherKey);
+
+    printf("Xored    ");
+    printBlock(&plaintext);
+
+    // p`lge%enxaoy,7J.
+    Block expected = 
+    { 
+        .bundles = 
+        {
+            0x70, 0x60, 0x6c, 0x67, 
+            0x65, 0x25, 0x65, 0x6e, 
+            0x78, 0x61, 0x6f, 0x79, 
+            0x2c, 0x37, 0x4a, 0x2e
+        }
+    };
+    printf("Expected ");
+    printBlock(&expected);
+    /*CipherData cipherData = { 0 };
     Block cipherKey = { 0x0, 0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7, 0x8, 0x9, 0xA, 0xB, 0xC, 0xD, 0xE, 0xF };
     Block block = { 0 };
     int i;
@@ -36,5 +85,6 @@ int main()
 
     // Affichage du résultat (normalement identique au clair)
     printf("Plain  ");
-    printBlock(&block);
+    printBlock(&block);*/
+    return EXIT_SUCCESS;
 }
